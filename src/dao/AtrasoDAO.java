6a032556ae3f2353aa01fb7f227cff11c73eafc0
eitiagoabreu <eitiagoabreu@gmail.com>
2021-02-26 03:27:44 -0300

    Versao 2

--- src/dao/AtrasoDAO.java
+++ src/dao/AtrasoDAO.java
@@ -16,38 +16,20 @@ public class AtrasoDAO {
 	
 	public String consultar()
 	{
-		String consultar = "SELECT * FROM (\r\n"
-				+ "SELECT h.entrada AS ex1, m.entrada AS ex2\r\n"
-				+ "FROM tblMarcacoesFeitas AS m, tblHorarioTrabalho AS h\r\n"
-				+ "WHERE m.entrada > h.entrada AND m.saida < h.saida\r\n"
-				+ "ORDER BY h.entrada ASC\r\n"
-				+ "LIMIT 1\r\n"
-				+ ") AS E1\r\n"
+		String consultar = "SELECT CASE WHEN (SELECT maux.saida FROM tblmarcacoesfeitas AS maux WHERE maux.id = m.id-1 AND maux.saida > h.entrada) < m.entrada THEN (SELECT maux.saida FROM tblmarcacoesfeitas AS maux WHERE maux.id = m.id-1) ELSE h.entrada END AS ea1, m.entrada AS ea2 \r\n"
+				+ "FROM tblmarcacoesfeitas AS m, tblHorarioTrabalho AS h\r\n"
+				+ "WHERE m.entrada > h.entrada AND m.entrada < h.saida \r\n"
+				+ "AND ((CASE WHEN m.entrada >= 0 AND m.saida <= 12 THEN 'AM' WHEN m.entrada > 12 AND m.saida <= 18  THEN 'PM' ELSE 'PN' END) = (CASE WHEN h.entrada >= 00 OR h.saida <= 12   THEN 'AM' \r\n"
+				+ "WHEN h.entrada > 12 AND h.saida <= 18  THEN 'PM' ELSE 'PN' END) \r\n"
+				+ "AND (SELECT COUNT(*) FROM tblmarcacoesfeitas) <= 1)\r\n"
 				+ "UNION ALL\r\n"
-				+ "SELECT * FROM (\r\n"
-				+ "SELECT m.saida AS ex1, h.saida AS ex2\r\n"
-				+ "FROM tblMarcacoesFeitas AS m, tblHorarioTrabalho AS h\r\n"
-				+ "WHERE m.saida < h.saida\r\n"
-				+ "ORDER BY m.saida DESC\r\n"
-				+ "LIMIT 1\r\n"
-				+ ") AS E2\r\n"
-				+ "UNION ALL\r\n"
-				+ "SELECT * FROM (\r\n"
-				+ "SELECT h.entrada AS ex1, m.entrada AS ex2\r\n"
-				+ "FROM tblMarcacoesFeitas AS m, tblHorarioTrabalho AS h\r\n"
-				+ "WHERE ((m.entrada < h.entrada AND m.saida < m.entrada) \r\n"
-				+ "AND ((h.entrada > 12 AND h.saida > 12) OR (h.entrada > 12 AND m.entrada < 12)))\r\n"
-				+ "OR \r\n"
-				+ "((m.entrada < h.entrada AND m.saida > m.entrada AND m.entrada > h.saida) \r\n"
-				+ "AND ((h.entrada > 12 AND m.entrada < 12) OR (h.entrada > 12 AND m.entrada < 12)))\r\n"
-				+ " OR \r\n"
-				+ "((m.entrada < h.entrada AND m.saida > m.entrada AND m.entrada < h.entrada and h.entrada > h.saida) \r\n"
-				+ "AND ((h.entrada > 12 AND m.entrada < 12)))\r\n"
-				+ "ORDER BY h.entrada ASC\r\n"
-				+ "LIMIT 1\r\n"
-				+ ") AS E3\r\n"
-				+ "";
-		
+				+ "SELECT m.saida AS ea1,h.saida AS ea2 \r\n"
+				+ "FROM tblmarcacoesfeitas AS m, tblHorarioTrabalho AS h \r\n"
+				+ "WHERE m.saida < h.saida AND m.entrada <= h.saida AND ((CASE WHEN m.entrada >= 0 AND m.saida <= 12 OR (SELECT COUNT(*) FROM tblmarcacoesfeitas) <= 1 THEN 'AM' \r\n"
+				+ "WHEN m.entrada > 12 AND m.saida <= 18 OR (SELECT COUNT(*) FROM tblmarcacoesfeitas) <= 1 THEN 'PM' ELSE 'PN' END) = (CASE WHEN h.entrada >= 0 AND h.saida <= 12   THEN 'AM' \r\n"
+				+ "WHEN h.entrada > 12 AND h.saida <= 18   THEN 'PM' ELSE 'PN' END) \r\n"
+				+ "OR (SELECT COUNT(*) FROM tblmarcacoesfeitas) <= 1)";
+			
 		return consultar;
 	}
 }
